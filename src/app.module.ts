1b26709d925ec06f04f2b3ee64074da3a6d734fb
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-11-29 20:13:32 -0600

    Import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from '@cfg/app-config.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
