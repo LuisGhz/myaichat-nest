1087c8cd2e18faae54eed748a0ed6acdc1f8af4e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-10 16:02:16 -0600

    feat(common): setup cache module with Redis store in CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,17 +2,31 @@ import { Global, Module } from '@nestjs/common';
 import { AuthModule } from '@auth/auth.module';
 import { JwtGuard } from './guards';
 import { APP_GUARD } from '@nestjs/core';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+import { EnvService } from '@cfg/schema/env.service';
+import { AppCacheService } from './services/app-cache.service';
 
 @Global()
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    CacheModule.registerAsync({
+      inject: [EnvService],
+      useFactory: async (envService: EnvService) => ({
+        ttl: envService.cacheTTL,
+        store: createKeyv(envService.redisHost),
+      }),
+    }),
+  ],
   providers: [
     JwtGuard,
     {
       provide: APP_GUARD,
       useClass: JwtGuard,
     },
+    AppCacheService,
   ],
-  exports: [JwtGuard],
+  exports: [JwtGuard, AppCacheService],
 })
 export class CommonModule {}
