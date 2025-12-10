439813eb9e6dd1ac7d78992d2bc5ed1078d5f5b6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-10 16:02:19 -0600

    feat(models): integrate caching in models controller

--- src/modules/models/models.controller.ts
+++ src/modules/models/models.controller.ts
@@ -20,19 +20,31 @@ import {
   ModelResDto,
   ModelListItemResDto,
 } from './dto';
+import { AppCacheService } from '@cmn/services/app-cache.service';
+import { CACHE_KEYS } from '@cmn/consts/cache.const';
 
 @Controller('models')
 export class ModelsController {
-  constructor(private readonly modelsService: ModelsService) {}
+  constructor(
+    private readonly modelsService: ModelsService,
+    private readonly appCacheService: AppCacheService,
+  ) {}
 
   @Post()
   async create(@Body() dto: CreateModelReqDto): Promise<CreateModelResDto> {
+    this.appCacheService.del(CACHE_KEYS.MODELS_FIND_ALL);
     return this.modelsService.create(dto);
   }
 
   @Get()
   async findAll(): Promise<ModelListItemResDto[]> {
-    return this.modelsService.findAll();
+    const cachedData = await this.appCacheService.get<ModelListItemResDto[]>(
+      CACHE_KEYS.MODELS_FIND_ALL,
+    );
+    if (cachedData) return cachedData;
+    const data = await this.modelsService.findAll();
+    await this.appCacheService.setShort(CACHE_KEYS.MODELS_FIND_ALL, data);
+    return data;
   }
 
   @Get('by-value')
@@ -50,12 +62,14 @@ export class ModelsController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() dto: UpdateModelReqDto,
   ): Promise<UpdateModelResDto> {
+    this.appCacheService.del(CACHE_KEYS.MODELS_FIND_ALL);
     return this.modelsService.update(id, dto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    this.appCacheService.del(CACHE_KEYS.MODELS_FIND_ALL);
     return this.modelsService.remove(id);
   }
 }
