f6e33a3f08ee8423d125672bd5100d6af3d00f8c
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-12 06:35:40 -0600

    feat(user): add controller endpoints for user and role management

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,48 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+} from '@nestjs/common';
+import { CurrentUser } from '@cmn/decorators';
+import type { JwtPayload } from '@cmn/interfaces';
+import { UserService } from './services';
+import {
+  GetAllRolesResDto,
+  GetAllUsersResDto,
+  UpdateUserRoleReqDto,
+  UpdateUserRoleResDto,
+} from './dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<GetAllUsersResDto> {
+    const users = await this.userService.findAll();
+    return { users };
+  }
+
+  @Get('roles')
+  async getAllRoles(): Promise<GetAllRolesResDto> {
+    const roles = await this.userService.findAllRoles();
+    return { roles };
+  }
+
+  @Patch(':id/update-role')
+  async updateUserRole(
+    @Param('id', ParseUUIDPipe) userId: string,
+    @Body() dto: UpdateUserRoleReqDto,
+    @CurrentUser() currentUser: JwtPayload,
+  ): Promise<UpdateUserRoleResDto> {
+    if (currentUser.sub === userId)
+      throw new BadRequestException("You can't update your own role");
+
+    await this.userService.updateUserRole(userId, dto.roleId);
+    return { message: 'User role updated successfully' };
+  }
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role, User } from './entities';
 import { RoleSeedService, UserService } from './services';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Role])],
+  controllers: [UserController],
   providers: [UserService, RoleSeedService],
   exports: [UserService],
 })
