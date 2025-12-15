27bc9a1fc822c80b68a46cb6cdcd7db29b985064
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-15 11:05:01 -0600

    Only accept images

--- src/modules/s3/consts/index.ts
+++ src/modules/s3/consts/index.ts
@@ -2,16 +2,12 @@ export const ALLOWED_FILE_TYPES = [
   'image/png',
   'image/jpeg',
   'image/jpg',
-  'application/pdf',
-  'text/plain',
 ] as const;
 
 export const ALLOWED_FILE_EXTENSIONS = [
   '.png',
   '.jpeg',
   '.jpg',
-  '.pdf',
-  '.txt',
 ] as const;
 
 export type AllowedFileType = (typeof ALLOWED_FILE_TYPES)[number];
